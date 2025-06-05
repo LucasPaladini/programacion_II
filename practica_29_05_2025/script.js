01d7d9f44ef2b65b4acc00f9ac93d6bbd73f6384
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2025-06-05 01:10:17 -0300

    Añadido ejercicio 4 y 5, corregido 3 y mejora del código

--- practica_29_05_2025/script.js
+++ practica_29_05_2025/script.js
@@ -16,3 +16,27 @@
         var color=document.getElementById('color');
         document.body.style.backgroundColor=color.value;
     }
+
+
+    // Ejer 4
+
+    let clics = 0;
+
+    function contadorClics() {
+        clics = clics + 1;
+        document.getElementById('contador').textContent = 'Clics: ' + clics;
+    }
+
+    // Ejer 4
+
+    // Ejer 5
+
+    function mostrarFecha() {
+        let fecha = new Date();
+        let actual = fecha.toLocaleString()
+        document.getElementById('fecha').textContent = "Fecha actual: " + actual;
+
+    }
+
+
+    // Ejer 5
