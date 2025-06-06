c619fb272e85562619fb87ce1b24544b2e689e8d
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2025-06-06 00:32:47 -0300

    Hecho hasta el ejercicio 9

--- practica_29_05_2025/script.js
+++ practica_29_05_2025/script.js
@@ -48,3 +48,23 @@
     }
 
     // Ejer 6
+
+    // Ejer 7
+
+    function mostrarBienvenida() {
+        let nombre = document.getElementById('bienvenida').value;
+        alert("Bienvenido "+ nombre + "!!!");
+    }
+
+    // Ejer 7
+
+    // Ejer 9
+
+    function cambiarMedidaFuente() {
+        let medida = document.getElementById("medida").value;
+        let texto = document.getElementById("texto");
+        texto.style.fontSize = medida;
+    }
+
+
+    // Ejer 9
