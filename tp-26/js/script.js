b036852321a6b7cc60f24c09bb614853073128f8
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2025-06-26 21:55:24 -0300

    eliminado repetidos

--- tp-26/js/script.js
+++
@@ -1,7 +0,0 @@
-function tomarDatos () {
-    let mensaje = document.getElementById("mensaje").value;
-    localStorage.setItem("mensaje", mensaje);
-    let recolectar = localStorage.getItem("mensaje");
-    document.getElementById("mensaje").innerText = recolectar;
-    alert(mensaje);
-}
