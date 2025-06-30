cf60d2a0dfd5f2a1e9c4eb87d42eb85e89dfaf42
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2025-06-29 23:29:17 -0300

    Terminado js

--- parcial/js/script.js
+++ parcial/js/script.js
@@ -1,33 +1,37 @@
-function ingresarRecomendacion() {
-    var nombre = document.getElementById("name").value;
-    var comentario = document.getElementById("mensaje").value;
-    var fecha = new Date().toLocaleDateString();
+enviarDatos()
 
-    localStorage.setItem("nombre", nombre);
-    localStorage.setItem("comentario", comentario);
-    localStorage.setItem("fecha", fecha)
+function tomarDatos() {
+    let name = document.getElementById("name").value;
+    let mensaje = document.getElementById("mensaje").value;
+    let fecha = new Date().toLocaleString();
 
+    localStorage.setItem("name", name);
+    localStorage.setItem("mensaje", mensaje);
+    localStorage.setItem("fecha", fecha);
 
-}
-mostrarRecomendacion()
-
-function mostrarRecomendacion() {
-    var nombre = localStorage.getItem("nombre");
-    var comentario = localStorage.getItem("comentario");
-    var fecha = localStorage.getItem("fecha");
-
-    var divNombre = document.createElement("div");
-    divNombre.className = "datos_detalle1";
-    divNombre.innerHTML = nombre + "<br>" + fecha;
-
-    var divComentario = document.createElement("div");
-    divComentario.className = "datos_detalle2";
-    divComentario.innerHTML = comentario;
-
-    let contenedor = document.getElementById("contenedorRecomendaciones");
-    contenedor.appendChild(divNombre);
-    contenedor.appendChild(divComentario);
-
+    enviarDatos()
 }
 
+    function enviarDatos () {
 
+    let name = localStorage.getItem("name");
+    let mensaje = localStorage.getItem("mensaje");
+    let fecha = localStorage.getItem("fecha");
+
+
+    const divPadre = document.querySelector(".nav-datos");
+
+    const nuevoDetalle1 = document.createElement("div");
+    nuevoDetalle1.className = "datos detalle1";
+    nuevoDetalle1.innerHTML = `
+      <p>${name}</p>
+      <p>${fecha}</p>
+    `;
+
+    const nuevoDetalle2 = document.createElement("div");
+    nuevoDetalle2.className = "datos detalle2";
+    nuevoDetalle2.innerHTML = `${mensaje}`;
+
+    divPadre.appendChild(nuevoDetalle1);
+    divPadre.appendChild(nuevoDetalle2);
+}
